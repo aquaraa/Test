5ebba1c123e5f473c828c7a869d2ca2f9f5ae245
Allicia Viona <alliciawn@gmail.com>
2025-03-24 06:24:43 +0700

    Initial Commit

---
+++ AboutMe-Advanced/AboutMe_AdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMe_AdvancedApp.swift
+//  AboutMe-Advanced
+//
+//  Created by Allicia Viona Sagi on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMe_AdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
